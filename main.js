656d3b770196cb3d87b8a99597e5aa7bf5e549af
sheodd <59453737+sheodd@users.noreply.github.com>
2020-01-03 13:01:15 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ var Health = 10;
 var hmult = 0;
 var damageUpgrades = 0;
 var plat = 0;
+//called whenever you click or need money per second
 function moneyClick(number){
 	moneys = (moneys + number);
 	
 	document.getElementById("moneys").innerHTML = moneys;
 	
 };
+//basic shop and mps gen base for how i did the rest
 function buyShop(){
 	var shopCost = Math.floor(10 * Math.pow(1.2,shops));
 	if(moneys >= shopCost){
@@ -27,6 +29,7 @@ function buyShop(){
 	var nextCost = Math.floor(10 * Math.pow(1.2,shops));
 	document.getElementById("shopCost").innerHTML = nextCost;
 };
+//bas upgade for shop how ill do upgrades from now on unless i finde a nuther way
 function buyShopUp(){
 	var shopUpCost = Math.floor(100 * Math.pow(2.1,shopups));
 	if(moneys >= shopUpCost) {
@@ -38,6 +41,7 @@ function buyShopUp(){
 	var shopUpCost = Math.floor(100 * Math.pow(2.1, shopups));
 	document.getElementById('shopUpCost').innerHTML = shopUpCost;
 };
+//clock that ticks every second and calls functions
 window.setInterval(function(){
 moneyClick((shops / 2) + (shops * shopups) / 2);
 MPS();
@@ -46,15 +50,18 @@ MPS();
 
 
 }, 1000);
+//calculats the money gained persecond based off of the upgrades you have with your shop and other things as thay are added
 function MPS(){
 var moneyPerSecond = ((shops / 2) + (shops * shopups) /2);
 moneyPS = moneyPerSecond;
 document.getElementById('moneyPS').innerHTML = moneyPS
-Convert();
+//Convert();
 };
+// going to be auto save featcher when i get there
 window.setInterval(function(){
 Save
 }, 1000);
+//amount per click mult
 function buyClickUpgrade(){
 var clickUpgradeCost = Math.floor(10 * Math.pow(2.1,clickupgrade));
 if (moneys >= clickUpgradeCost){
@@ -67,6 +74,7 @@ if (moneys >= clickUpgradeCost){
 	document.getElementById('clickUpgradeCost').innerHTML = clickUpgradeCost;
 };
 };
+// saveing var
 
 function Save(){
 	var save = {
@@ -83,6 +91,7 @@ function Save(){
 localStorage.setItem("save",JSON.stringify(save));
 
 };
+// well you should know what it is but its a cheat that makes you have way to much 
 function Cheat(){
 	moneys = moneys + 1000000000000000;
 	document.getElementById('moneys').innerHTML = moneys;
@@ -98,6 +107,7 @@ function Gold(){
 	gold = gold + 150;
 	document.getElementById('gold').innerHTML = gold;
 };
+// calculats hp loss and gain
 function HP(){
 	
 
@@ -114,6 +124,7 @@ if (Health <= 0){
 };
 
 };
+// upgrade for damage
 function buyDamageUpgrade(){
 	var damageUpgradeCost = Math.floor(10 * Math.pow(2.1,damageUpgrades));
 	if (moneys >= damageUpgradeCost){
@@ -129,7 +140,8 @@ function buyDamageUpgrade(){
 	document.getElementById('damage').innerHTML = damage;
 
 };
-function Convert(){
+//converts curency
+/*function Convert(){
 	if (moneys >= 100){
 		moneys = moneys - 100;
 		gold = gold + 1;
@@ -143,10 +155,16 @@ function Convert(){
 	document.getElementById('plat').innerHTML = plat;
 	document.getElementById('gold').innerHTML = gold;
 	};
+		*/
 		
-};
+//};
 
 
+
+
+
+
+// loads game
 function Load(){
 	var savegame = JSON.parse(localStorage.getItem("save")); 
 	if (typeof savegame.hmult !== "undefined") hmult = savegame.hmult;
@@ -158,6 +176,7 @@ function Load(){
 	if (typeof savegame.shopups !== "undefined") shopups = savegame.shopups; document.getElementById('shopups').innerHTML = shopups;var shopUpCost = Math.floor(100 * Math.pow(1.1, shopups));document.getElementById('shopUpCost').innerHTML = shopUpCost;
 	if (typeof savegame.clickupgrade !== "undefined") clickupgrade = savegame.clickupgrade; document.getElementById('clickupgrade').innerHTML = clickupgrade;var clickUpgradeCost = Math.floor(10 * Math.pow(2.1,clickupgrade));document.getElementById('clickUpgradeCost').innerHTML = clickUpgradeCost;
 };
+// delets the save of the game for good unless you save a gin
 function delSave(){
 	localStorage.removeItem("save");
 	location.reload();
